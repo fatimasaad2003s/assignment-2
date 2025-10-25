7463f3aa041df4f8f8e8962b42ee4ac7bc125151
fatimasaad2003 <s202268600@kfupm.edu.sa>
2025-10-25 20:05:17 +0300

    dynamic and data

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ themeToggle.addEventListener('click', () => {
   }
 });
 
+
+const greetingElement = document.getElementById("greeting");
+
+let userName = localStorage.getItem("userName");
+
+if (!userName) {
+  userName = prompt("Welcome! What is your name?");
+  
+  if (userName) {
+    localStorage.setItem("userName", userName);
+  }
+}
+
+function getTimeNow() {
+  const hour = new Date().getHours();
+  if (hour < 12) return "Good morning";
+  else if (hour < 18) return "Good afternoon";
+  else return "Good evening";
+}
+
+if (userName) {
+  greetingElement.textContent = `${getTimeNow()}, ${userName}! `;
+} else {
+  greetingElement.textContent = "Welcome to my website! ";
+}
+
+
